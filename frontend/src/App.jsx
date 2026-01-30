182a67db44dbdd9e634ed839b97f067a4324a766
GaganKalra <135148767+GaganKalra@users.noreply.github.com>
2026-01-30 19:10:15 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Footer from './components/Footer'
 function App() {
